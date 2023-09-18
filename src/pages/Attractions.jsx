fcce3c758b9bbbb669a3995494c869a32dca10b2
Michał Murawski <m.a.murawski@wp.pl>
2023-09-18 15:14:06 +0200

    add routes / pages / dashboard layout

---
+++ src/pages/Attractions.jsx
@@ -0,0 +1,9 @@
+const Attractions = () => {
+  return (
+    <div style={{ backgroundColor: 'lightgray', padding: '30px' }}>
+      <h2>Atrakcje</h2>
+    </div>
+  );
+};
+
+export default Attractions;
