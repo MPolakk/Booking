6dd1e12f75e40fd414cf28dda2fb6d90a2b8324a
AnimaVillis <92624844+AnimaVillis@users.noreply.github.com>
2023-09-21 16:57:05 +0200

    Added docker.
    W razie problemu z uruchomieniem lub potrzebną instrukcją zapraszam na kanał.

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: 'lightgray',
+        padding: '30px',
+      }}
+    >
+      <h2>O nas</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div style={{ backgroundColor: 'lightgray', padding: '30px' }}>
+      <h2>Kontakt</h2>
+      <h3 id={'rezerwacja'}>Rezerwacja</h3>
+    </div>
+  );
+};
+
+export default Contact;
