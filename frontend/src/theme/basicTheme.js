2098eaff37dcb1dedaf1d14f39172e20279260d2
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-10-07 17:38:29 +0200

    Add needed font files and an empty basicTheme file.

---
+++ frontend/src/theme/basicTheme.js
@@ -0,0 +1,13 @@
+@font-face {
+  font-family: myFirstFont;
+  src: url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet');
+}
+
+
+
+export const basicTheme = {
+  font: {},
+  fontSize: {},
+  fontWeight: {},
+  fontColor: {},
+};
