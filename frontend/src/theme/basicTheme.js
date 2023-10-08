15ee93c97b2a3bc3910e6f8635d771aa7a850871
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-10-08 17:51:46 +0200

    Add new needed Montserrat font and basicTheme file with some font options.

--- frontend/src/theme/basicTheme.js
+++ frontend/src/theme/basicTheme.js
@@ -1,13 +1,28 @@
-@font-face {
-  font-family: myFirstFont;
-  src: url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet');
-}
-
-
-
 export const basicTheme = {
-  font: {},
-  fontSize: {},
-  fontWeight: {},
-  fontColor: {},
+  font: {
+    main: 'OpenSans-Regular-400',
+    mainBold: 'OpenSans-Bold-700',
+    titlePrimary: 'Montserrat-Bold-700',
+    titleSecondary: 'PlayfairDisplay-BlackItalic-700',
+    buttonForm: 'Montserrat-Bold-700',
+    buttonCard: 'Montserrat-Regular-400',
+    label: 'Montserrat-Bold-700',
+    input: 'OpenSans-Regular-400',
+    footer: 'OpenSans-Regular-400',
+  },
+  fontSize: {
+    main: '14px',
+    buttonForm: '11px',
+    buttonCard: '10px',
+    input: '13px',
+    label: '10px',
+    footer: '13px',
+    titleH2small: '27px',
+    titleH2big: '40px',
+    titleH3: '30px',
+    titleH4small: '16px',
+    titleH4big: '17px',
+    titleH5: '20px',
+    titleH6: '15px',
+  },
 };
