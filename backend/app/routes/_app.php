597f325d8528c78d938955fd57f1cefbdf08345d
AnimaVillis <92624844+AnimaVillis@users.noreply.github.com>
2023-10-12 11:22:23 +0200

    Fixed not working nginx.

--- backend/app/routes/_app.php
+++ backend/app/routes/_app.php
@@ -3,3 +3,8 @@
 app()->get('/', function () {
     response()->json(['message' => 'Congrats!! You\'re on Leaf API']);
 });
+
+app()->get('/wilku', function () {
+    response()->json(['message' => 'Wilku uczy sie PHP i Leafa']);
+});
+
